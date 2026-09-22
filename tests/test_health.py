2dfa00de03d9a8f507ea75e761c03d1078f9a4b2
Rameshwar Katoch <rameshwarkatoch@Rameshwars-MacBook-Air.local>
2026-09-22 11:32:14 +0530

    Add root and version endpoint tests

--- tests/test_health.py
+++ tests/test_health.py
@@ -9,3 +9,21 @@ def test_health_endpoint():
 
     assert response.status_code == 200
     assert response.json() == {"status": "Active"}
+
+
+def test_root_endpoint():
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "This is the Release Guard"
+    }
+
+
+def test_version_endpoint():
+    response = client.get("/version")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "version": "0.0.1"
+    }
